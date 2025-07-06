34778778905da96d96c07b62706dcd0933845902
purvawaskar <waskarpurva@gmail.com>
2025-07-05 18:45:23 -1200

    operators

---
+++ DAY_3/script.js
@@ -0,0 +1,72 @@
+ // operators in javascript
+
+ // 1.Arithemetic opetators 
+ // +,-,*,/,%
+
+ let first_number=10
+ let second_number=20
+ console.log(first_number+second_number)
+
+ // 2. Substration operator
+
+   let num1 = 15
+   let num2 = 5
+   console.log(num1-num2)
+
+ // 3. Multiplaction operator
+
+    let a = 10
+    let b =  5
+    console.log(a*b)
+
+ // 4. division operator 
+
+ let x = 5
+ let y = 2
+ console.log(x/y)
+
+ // module operators
+
+ let number1 = 10
+ let number2 = 5
+ console.log(number1%number2)
+
+
+ // 2. Assignment operator
+ // == assign operator
+
+ let num = 12
+ console.log(num)
+
+ let abc = 15
+ let xyz = abc
+ console.log(xyz)
+
+ // += add or assign
+
+ let z = 10
+  z = z+1
+  console.log(z)
+
+// -= substract or assign
+
+ let num_1 = 20
+  num_1 = num_1 - 2
+  console.log(num_1)
+
+// *= multiply or assign
+
+  let num_2 = 30
+   num_2 = num_2 * 3
+   console.log(num_2)
+
+// /= division or multiply
+
+   let num3 = 25
+   num3 = num3 / 5
+   console.log(num3)
+
+
+ 
+
+
