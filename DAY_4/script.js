008cd3c875519876d544564d6a7826ef045baf4e
purvawaskar <waskarpurva@gmail.com>
2025-07-13 23:54:12 -1200

    popup box

---
+++ DAY_4/script.js
@@ -0,0 +1,22 @@
+// Popup Box
+
+// alert box
+
+alert("This is alert box")
+
+// prompt Box
+
+
+let first_1 = Number (prompt("enter first number"))
+
+let second_1 = Number (prompt("enter second number"))
+
+
+console.log(first_1 + second_1)
+
+
+// confirm
+
+confirm("are you sure")
+
+confirm("are you confirm")
