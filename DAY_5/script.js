dbbbc21721fd68073bca908cf10465b764f1a336
purvawaskar <waskarpurva@gmail.com>
2025-07-18 22:17:03 -1200

    conditional statement if-else

---
+++ DAY_5/script.js
@@ -0,0 +1,47 @@
+// conditional statement
+
+// 1. if statement
+
+ let num = 1
+
+ if(num==1){
+
+    console.log("condition is true")
+ }
+
+
+ let user_name = "purva"
+
+ if(user_name == "pooja"){
+    console.log("login sucessfully")
+ }
+
+ let n = 20
+
+ if(n % 2 == 0){
+    console.log("number is even")
+
+ }
+
+ if(n % 2 !==0){
+    console.log("number is odd")
+ }
+
+ // 2. if-else
+
+  let age = 25
+
+  if(age>=18){
+   console.log("eligible for votiong")
+
+  }else{
+   console.log("not eligible for voting")
+  }
+
+  let number = 3
+
+  if(number % 2 == 0){
+    console.log(number,'is even')
+  }else{
+    console.log(number,'is odd')
+  }
