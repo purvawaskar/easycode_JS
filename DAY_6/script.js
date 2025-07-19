e8f5da4623380965f2d36d0d194f68c61f6cdeab
purvawaskar <waskarpurva@gmail.com>
2025-07-18 22:53:57 -1200

    switch statement

---
+++ DAY_6/script.js
@@ -0,0 +1,55 @@
+// switch statement
+
+ let day = "monday";
+
+switch (day) {
+    case "monday": 
+        console.log("monday");
+        break; 
+    case "tuesday": 
+        console.log("tuesday");
+        break; 
+    case "wednesday":
+        console.log("wednesday");
+        break; 
+    default:
+        console.log("invalid day");
+}
+
+ let fruit = "mango"
+
+ switch (fruit){
+    case "orange":
+        console.log("orange");
+        break;
+    case "apple":
+        console.log("apple");
+        break;
+    case "mango":
+        console.log("mango")
+        break;
+    case "grapes":
+        console("grapes")
+    default:
+        console.log("not found");
+    
+ } 
+
+ let flower = prompt("enter flower name")
+  
+ switch (flower){
+    case "sunflower":
+        console.log("sunflower");
+        break;
+    case "rose":
+        console.log("rose");
+        break;
+    case "lily":
+        console.log("lily")
+        break;
+    case "jasmine":
+        console("jasmine")
+    default:
+        console.log("not found");
+    
+ } 
