be9bfcc49b3ef91b4e65f339aded3a36edb23ff9
purvawaskar <waskarpurva@gmail.com>
2025-07-20 06:06:24 -1200

    while and do..while loop

---
+++ DAY_7/script.js
@@ -0,0 +1,36 @@
+// Loops in Javascript
+
+// 1. While loop
+
+let n = 0
+
+while(n<=10){
+    console.log("number", n)
+    n++
+}
+
+let i = 0
+
+while(i<5){
+    console.log(i)
+    i++
+}
+
+// print 0 to 100 even number 
+
+let count = 0
+
+while(count <= 100){
+    console.log(count)
+    count = count + 2
+}
+
+
+// do..while loop
+
+let j = 0
+
+do{
+    console.log(j)
+    j++
+}while(j<=10)
