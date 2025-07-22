e8ffd0517ad96955355eee3715181404458dc87f
purvawaskar <waskarpurva@gmail.com>
2025-07-21 18:53:51 -1200

    for loop

---
+++ DAY_8/script.js
@@ -0,0 +1,30 @@
+// for loop
+
+let i = 1
+
+for(i; i<=10; i++){
+    console.log(i)
+}
+
+// print the multiplaction table
+
+let n = 5
+
+for(let i = 1; i<=10; i++){
+    console.log(n*i)
+}
+
+// program to print the sum of 10 number
+
+let num = 1
+sum = 0
+
+for(num ; num<=10; num++){
+    sum=sum+num
+
+}
+console.log(sum)
+
+
+
+ 
