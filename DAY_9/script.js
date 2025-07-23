93d0913f1dd8a6661c75e240205ff68fe52e4a1e
purvawaskar <waskarpurva@gmail.com>
2025-07-22 21:18:01 -1200

    document object model

---
+++ DAY_9/script.js
@@ -0,0 +1,18 @@
+// Document Object Model in javascript (DOM)
+
+// document means whole html code page
+
+
+console.log(document)
+
+let title = document.title
+console.log(title)
+
+document.writeln("purva");
+
+
+
+
+
+
+
