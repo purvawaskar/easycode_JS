f9ac35fb8cbe90012d2f380d2cd8c46664aca695
purvawaskar <waskarpurva@gmail.com>
2025-07-22 21:57:21 -1200

    Array and method

---
+++ DAY_10/script.js
@@ -0,0 +1,46 @@
+// Array
+// HOW to create Array
+
+let arr = [1,2,3,4,5]
+console.log (arr)
+
+
+let fruits = ["mango","grapes","pineapple","apple"]  // array
+console.log(fruits)
+console.log(fruits[2])  // index of array
+
+
+
+let array = [1,2,3,4,5,6,7,8,9]
+
+console.log(array) // print array
+
+console.log(array.length) // find the length of array
+
+
+// methods of array
+// 1. push method ( which add the element at the end of array)
+
+let flowers= ["rose","lily","lotus","sunflower"]
+
+console.log (flowers)
+
+flowers.push("mogra")  // push method
+
+
+let a = [1,2,3,4,5,6]
+
+console.log(a)
+
+a.push(7)
+
+
+// pop method
+
+let students = ["sanika","purva","pooja","siddhi"]
+
+console.log(students)
+
+students.pop()
+
+
