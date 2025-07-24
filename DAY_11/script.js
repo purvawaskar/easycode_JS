b203a25a2359a5a07fcd99c84ea5f8b051569576
purvawaskar <waskarpurva@gmail.com>
2025-07-24 05:46:42 -1200

    shift,unshift,splice method of array

---
+++ DAY_11/script.js
@@ -0,0 +1,45 @@
+// array methods
+
+//unshift method
+
+// shift method delete first element of array
+
+let numbers=[1,2,3,4,5]
+console.log(numbers)
+numbers.shift()     // unshift method
+
+
+let cars=["nexon","safari","rangerover","creta"]
+console.log(cars)
+cars.shift()  
+
+// unshitf() method
+
+// unshift method add new element at startof aaray
+
+let cityName=["pune","mumbai","delhi"]
+console.log(cityName)
+cityName.unshift("banglore")
+
+
+let array=[1,2,3,4,5,6,7]
+console.log(array)
+array.unshift(8)
+
+let flowers=["rose","lotus","sunflower"]
+console.log(flowers)
+flowers.unshift("lily")
+
+
+// splice method
+
+let id =[11,12,13,14,15]
+console.log(id)
+id.splice(2, 1)
+
+let a = ["Apple", "Banana", "Cherry", "Date"];
+a.splice(1, 2);
+
+console.log(a);
+
+
