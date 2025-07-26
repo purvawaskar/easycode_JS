d6cb228d7f676985f7682e1bca4b987e49a4a374
purvawaskar <waskarpurva@gmail.com>
2025-07-26 05:37:14 -1200

    object method and for Of loop and for in loop

--- DAY_13/script.js
+++ DAY_13/script.js
@@ -32,4 +32,45 @@ const mobile={
 }
 let results=Object.entries(mobile)
 
+// freeze method
+
+let bankaccount = {
+    name: "Purva waskar",
+    account_no: 234785859,
+    balance: 500000,
+    bank_name: "SBI"
+}
+Object.freeze(bankaccount);
+
+// This line will have no effect because the object is 
+
+bankaccount.balance = "100000";
+console.log(bankaccount);
+
+
+// for Of loop
+
+let colors=["red","blue","red","orange"] 
+for(let i of colors){
+    console.log(i)
+}
+
+
+let numbers=[1,2,3,4,5,6]
+for (const j of numbers) {
+    console.log(j)
+}
+
+
+//for in loop
+
+const person2 = {
+      name: "mrunal",
+      age: 19
+    };
+    for (const element in person2) {
+      console.log(element); 
+    }
+
+
 
