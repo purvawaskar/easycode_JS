68c5f4399ff62d2ebb15e20abcfee6d486bcb970
purvawaskar <waskarpurva@gmail.com>
2025-07-26 03:02:05 -1200

    intoduction to object

---
+++ DAY_12/script.js
@@ -0,0 +1,31 @@
+// Introduction to Object
+// how to create object
+
+let person={
+    name:"purva",
+    address:"jejuri",
+    clg_name:"waghire college saswad"
+
+}
+console.log(person)
+
+person.gender="female"   // add key and value in object
+
+
+let student_info={
+    s_id:101,
+    name:"sanika",
+    marks:20
+}
+console.log(student_info)
+console.log(student_info.name)  // get a particular value of key
+
+
+const insta_user={
+    name:"Purvaa",
+    username:"purva_1701"
+}
+
+console.log(insta_user)
+console.log(insta_user.name)
+console.log(insta_user.username)
