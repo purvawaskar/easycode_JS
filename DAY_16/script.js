e204982afaed754258a8d22cd32a8868fa448a5c
purvawaskar <waskarpurva@gmail.com>
2025-07-29 19:10:07 -1200

    DOM Accessing element

---
+++ DAY_16/script.js
@@ -0,0 +1,20 @@
+// Document object model
+
+// Accessing elememt by dom
+
+// // get element by tag name
+
+let res=document.getElementsByTagName("h1")
+console.log(res)
+
+let result=document.getElementsByTagName("h2")
+console.log(result)
+
+
+// get element by classname
+
+let res1=document.getElementsByClassName("container")
+console.log(res1)
+
+let result1=document.getElementsByClassName("container1")
+console.log(result1)
