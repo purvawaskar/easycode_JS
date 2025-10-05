310fb85ebfa07c242555a0c4817abce3d177eee5
purvawaskar <waskarpurva@gmail.com>
2025-10-05 15:21:23 +0530

    javascript events (mousemove , mouseleave , mouseenter)

--- DAY_19/script.js
+++ DAY_19/script.js
@@ -1,9 +1,23 @@
 let container=document.getElementById("container");
 let shadow=document.getElementById("moving-shadow")
 
+
+
 document.addEventListener("mousemove",(e)=>{
     shadow.style.left=`${e.clientX/5}px`;
     shadow.style.top=`${e.clientY/5}px`;
+})
+
+
+document.addEventListener("mouseenter",(e)=>{
+    shadow.style.backgroundColor="rgba(255, 6, 6, 0.6)";
+   
+})
+
+document.addEventListener("mouseleave",(e)=>{
+    shadow.style.backgroundColor="rgba(9, 66, 152, 1)";
+    
+
 })
 
 
