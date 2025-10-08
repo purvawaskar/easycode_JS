23155ffffcf163f2e95f65cb3106f22dbbdefbeb
purvawaskar <waskarpurva@gmail.com>
2025-10-08 21:21:52 +0530

    local storage and JSON

--- DAY_22/script.js
+++ DAY_22/script.js
@@ -7,3 +7,16 @@ let person = {
 }
 
 let res=JSON.stringify(person)
+console.log(res)
+
+
+
+let res2=JSON.parse(res);
+console.log(res2);
+
+//Local storage which provided by browser
+
+
+localStorage.setItem("user_name","purva")
+
+localStorage.setItem("user_age","21")
